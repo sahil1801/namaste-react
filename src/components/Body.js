1102a716e50b90b6f0ba4a92c7030492efad9d50
sahil Savaliya <sahilsavaliya1801@gmail.com>
2023-12-19 13:51:17 +0530

    solved some errors

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,20 +19,26 @@ export const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-
-    const json = await data.json();
-
-    setListOfRestaurants(
-      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-    setFilteredRestaurant(
-      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+  
+      if (!data.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const json = await data.json();
+  
+      setListOfRestaurants(
+        json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      );
+      setFilteredRestaurant(
+        json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      );
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
-
 
   const onlineStatus = useOnlineStatus();
   if (onlineStatus === false)
